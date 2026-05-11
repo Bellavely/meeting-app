42ae3c0e0d4d2d901a39f697fa2452e822e650d3
bella.vayzburg <bella.vaisborg@gmail.com>
2026-05-11 18:45:24 +0300

    added a limit for connecting my db

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -8,7 +8,8 @@ export const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT || '5432'),
+  port: parseInt(process.env.DB_PORT || '6543'),
+  max: 10,
 });
 
 export const query = (text: string, params?: any[]) => pool.query(text, params);
