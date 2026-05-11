74790250431e5d6b5a13645a6a4859cd428c7dfe
bella.vayzburg <bella.vaisborg@gmail.com>
2026-05-11 20:09:01 +0300

    updated port num

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,7 @@ import { runMigrations } from "./dal/db/init-db";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT ;
 const allowdOrigins = process.env.CLIENT;
 app.use(
   cors({
