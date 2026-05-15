de3027fda4df2f5e6059e0454ffe11d28fbd927e
bella.vayzburg <bella.vaisborg@gmail.com>
2026-05-15 16:29:51 +0300

    fixed cookie

--- backend/src/api/controllers/authController.ts
+++ backend/src/api/controllers/authController.ts
@@ -91,6 +91,8 @@ export const refresh = async (
 
     res.cookie("refreshToken", newRefreshToken, {
       httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
       maxAge: 30 * 24 * 60 * 60 * 1000,
       path: "/",
     });
